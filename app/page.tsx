724f6c0239fb88bfcfba5c3c7f6ca93650f8126f
Pierre Guillemot <pierre.guillemot@adveris.fr>
2023-08-28 09:52:32 +0200

    add smooth scroll with errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import PaddingContainer from '@/components/layout/padding-container'
 import PostCard from '@/components/post/post-card'
 import Image from 'next/image'
@@ -5,8 +6,26 @@ import Link from 'next/link'
 import { DUMMY_POSTS } from '@/DUMMY_DATA'
 import PostList from '@/components/post/post-list'
 import CTACard from '@/components/elements/cta-card'
+import {useEffect} from 'react'
+import Lenis from '@studio-freight/lenis'
+
 
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis()
+
+    // lenis.on('scroll', (e) => {
+    //   console.log(e)
+    // })
+    
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    
+    requestAnimationFrame(raf)
+  }, [])
+
   return (
     <PaddingContainer>
       <main className="h-auto space-y-10">
